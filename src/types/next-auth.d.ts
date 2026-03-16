7cf7b6f431c4110b2152193440e4e8c1c5482741
nutboonmee18 <nutboonmee18@gmail.com>
2026-03-16 18:27:32 +0700

    เตรียมฐานรอเชื่อม Database

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,15 +1,28 @@
 import "next-auth";
+import "next-auth/jwt";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
       username: string;
-    } & DefaultSession["user"];
+      name?: string | null;
+      email?: string | null;
+      roles: number[];
+    };
   }
 
   interface User {
     id: string;
     username: string;
+    roles: number[];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    username: string;
+    roles: number[];
   }
 }
